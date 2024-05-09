a33ae137f7147ea00e97d7598ddfabbb3cbd7242
Peter Bell <peterbell10@live.co.uk>
2024-05-09 01:15:47 +0000

    [inductor] Disable fp contraction and add precise division option
    Fixes #101039, Fixes #122179
    
    In the issue we have something like:
    ```python
    (x / 1e-6) + (y / 1e-6)
    ```
    which triton turns into
    ```python
    fma(x, 1e6, y * 1e6)
    ```
    
    Where we have one division calculated to float32 precision,
    and the other calculated to infinite precision as part of the `fma`.
    In the issue this leads to a bad normalization in softmax.
    
    Here we disable `fma` fusion in the trion compiler.
    
    ghstack-source-id: 139c7db6fca6930c4a02c41006e9b53daa5bbfd0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115435

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -361,6 +361,11 @@ class CachingAutotuner(KernelInterface):
                 "num_warps": compile_meta["num_warps"],
                 "num_stages": compile_meta["num_stages"],
                 "debug": compile_meta["debug"],
+                "enable_fp_fusion": (
+                    config.triton.enable_fp_fusion
+                    or self.heuristic_type
+                    in (HeuristicType.TEMPLATE, HeuristicType.USER_AUTOTUNE)
+                ),
             }
             if self.device_props.type != "hip":
                 if "waves_per_eu" in compile_meta:
